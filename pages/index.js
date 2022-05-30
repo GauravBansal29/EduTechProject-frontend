d6685529b1fad254b96beb60ffc6c07f1c2507e8
Gaurav Bansal <urrgb29@gmail.com>
2022-05-30 19:03:19 +0530

    Course dashboard update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
+import {useState, useEffect} from "react"
+import axios from 'axios'
+import {useRouter} from 'next/router'
+import CourseCard from "../components/Cards/CourseCard";
 const Index= ()=>{
+    const router= useRouter();
+    const [courses, setCourses]= useState([]);
+    useEffect(()=>{
+        const fetchCourses = async()=>{
+            try{
+            const {data}= await axios.get('/api/courses');
+            if(data) setCourses(data);
+            }
+            catch(err)
+            {
+                console.log(err);
+                toast("Some error occured");
+                router.push('/err');
+                
+            }
+
+        }
+        fetchCourses();
+        console.log(courses);
+    },[router.isReady])
     return (
     <>
-    <h1 className=" jumbotron text-center bg-primary linear-gradient text-white">Welcome to the next project</h1>
+    <h1 className=" jumbotron text-center bg-primary linear-gradient text-white">Welcome to CourseBay</h1>
+ 
+    <div className="container-fluid">
+    <div className="row">
+   { courses.map((course) => {
+        return(<div key={course._id} className="col-md-4" >
+       <CourseCard course={course} />
+       </div>);
+
+    }) }
+    </div>
+    </div>
     </>
     );
     
